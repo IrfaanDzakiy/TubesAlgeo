0f10234c573fa5429614c0d4c658bd3780078387
IrfaanDzakiy <52844931+IrfaanDzakiy@users.noreply.github.com>
2019-09-26 13:25:01 +0700

    driver input file

--- DriverMatriks.java
+++ DriverMatriks.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 class DriverMatriks{
 	public static void main(String[] args){
-		int n;
-		double hasil;
+		int m,n;
 		Scanner read = new Scanner(System.in);
+
+		System.out.print("Masukan m :");
+		m = read.nextInt();
 		System.out.print("Masukan n :");
 		n = read.nextInt();
 
-		Matriks M = new Matriks(n+1,n+2);
-		hasil = M.interpol(n);
-		System.out.print(hasil);
+		Matriks M = new Matriks(m,n+1);
+		M.bacaFileIntrapolasi("test.txt");
+		M.tulisMatriks();
+
+
 	}
 }
